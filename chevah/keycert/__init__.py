96ed076b2d6879ada5a37caf1994ba4d732bc6ee
Adi Roiban <adi.roiban@chevah.com>
2015-04-08 17:20:39 +0100

    update coverage.

--- chevah/keycert/__init__.py
+++ chevah/keycert/__init__.py
@@ -8,6 +8,8 @@ import sys
 def _path(path, encoding):
     if os.name != 'posix' or sys.platform.startswith('darwin'):
         # On Windows and OSX we always use unicode.
+        # We don't run yet tests on Windows and OSX.
+        # pragma: no cover
         return path
 
     return path.encode('utf-8')
